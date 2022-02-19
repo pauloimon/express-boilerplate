56213f628773376e55853811bfa9787c76481fb2
Paulo Imon <pauloimon@yahoo.com>
2022-02-19 17:04:25 -0300

    Add Express implementation

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import 'dotenv/config'
 import appConfig from './config/app.js'
+import app from './src/core/app.js'
+
+const { name, port } = appConfig
+
+app.listen(port, () => {
+  console.log(`> ${name}`)
+  console.log(`> App listening on port ${port}`)
+})
