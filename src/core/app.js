f46d5ebadc04ad4b356ccd8e9c713f9965bb1bb9
Paulo Imon <pauloimon@yahoo.com>
2022-02-19 17:08:23 -0300

    Add core module to resolve routes dynamically

--- src/core/app.js
+++ src/core/app.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import routesResolver from './routesResolver.js'
+
+const router = express.Router()
+routesResolver(router)
 
 const app = express()
+  .use(router)
 
 export default app
---
+++ src/core/routesResolver.js
@@ -0,0 +1,26 @@
+import apiRoutes from '../../routes/api.js'
+import webRoutes from '../../routes/web.js'
+
+export default router => {
+  const routes = [
+    ...apiRoutes.map(route => {
+      route.path = '/api' + route.path
+
+      return route
+    }),
+    ...webRoutes
+  ]
+
+  routes.forEach(({ methods, path, middlewares = [], action }) => {
+    if (methods === '*') {
+      router.all(path, ...middlewares, action)
+
+      return
+    }
+
+    methods.map(method => method.toLowerCase())
+      .forEach(method => {
+        router[method](path, ...middlewares, action)
+      })
+  })
+}
