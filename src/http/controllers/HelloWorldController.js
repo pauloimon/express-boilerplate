a17d28f04af0c2a498fbefb01023602bcb3887fb
Paulo Imon <pauloimon@yahoo.com>
2022-02-21 20:54:42 -0300

    Remove unnecessary return

--- src/http/controllers/HelloWorldController.js
+++ src/http/controllers/HelloWorldController.js
@@ -2,6 +2,6 @@ export default class {
   static handle (request, response) {
     console.log('> You called /api/hello-world route')
 
-    return response.json({ hello: 'World' })
+    response.json({ hello: 'World' })
   }
 }
