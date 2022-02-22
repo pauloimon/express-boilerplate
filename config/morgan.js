a37b124b0451b9468adffda85a8faa4dfcf5d275
Paulo Imon <pauloimon@yahoo.com>
2022-02-21 21:49:36 -0300

    Add morgan middleware with customized logs; Close #2

---
+++ config/morgan.js
@@ -0,0 +1,3 @@
+export default {
+  format: process.env.MORGAN_FORMAT || 'tiny'
+}
--- src/core/app.js
+++ src/core/app.js
@@ -1,6 +1,8 @@
 import cors from 'cors'
 import express from 'express'
+import morgan from 'morgan'
 import corsConfig from '../../config/cors.js'
+import morganConfig from '../../config/morgan.js'
 import routesResolver from './routesResolver.js'
 
 const router = express.Router()
@@ -8,6 +10,7 @@ routesResolver(router)
 
 const app = express()
   .use(cors(corsConfig))
+  .use(morgan(morganConfig.format))
   .use(router)
 
 export default app
